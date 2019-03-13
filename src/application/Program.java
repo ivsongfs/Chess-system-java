0bdc3deeab631c2ad3349ed72a1ba960aefc1332
ivsongfs <ivsongfs@gmail.com>
2019-03-13 04:38:02 -0300

    Novas implementacoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board bd = new Board(8, 8);
-		
-		System.out.println(bd);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
